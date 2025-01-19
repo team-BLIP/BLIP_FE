4b58d61ffa814b950d3d01b2aff574865e08f4be
seeum0507 <kwonstella0507@gmail.com>
2025-01-19 18:51:21 +0900

    초기세팅

---
+++ blip-project/src/style/color.jsx
@@ -0,0 +1,41 @@
+export const color = {
+  White: "#FFFFFF",
+
+  Black: "#000000",
+
+  Bg: "#F9FBFA",
+
+  Blue: "#238EFF",
+
+  Main: [
+    "#D6EFDF", // Main 50
+    "#ADDEBF", // Main 100
+    "#84CE9F", // Main 200
+    "#5ABE7F", // Main 300
+    "#31AD5F", // Main 400
+    "#278A4C", // Main 500
+    "#1F6E3D", // Main 600
+    "#195831", // Main 700
+    "#144627", // Main 800
+  ],
+
+  GrayScale: [
+    "#F2F2F2", // Gray 50
+    "#DEDEDE", // Gray 100
+    "#CACACA", // Gray 200
+    "#B5B5B5", // Gray 300
+    "#A1A1A1", // Gray 400
+    "#8C8C8C", // Gray 500
+    "#610064", // Gray 600
+    "#4C4B4F", // Gray 700
+    "#38373B", // Gray 800
+  ],
+
+  Error: [
+    "#FF4646", // Error 400
+    "#DD3232", // Error 300
+    "#991414", // Error 200
+    "#FFC3C3", // Error 100
+    "#FFF0F0", // Error 50
+  ],
+};
