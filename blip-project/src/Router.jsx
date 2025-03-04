469fde67df343f4c9dc07a3472ffd26aeb255d84
tccmw <jungmwoo6248@gmail.com>
2025-03-04 22:54:49 +0900

    main

--- blip-project/src/Router.jsx
+++ blip-project/src/Router.jsx
@@ -1,11 +1,17 @@
-import "./Router.css"
+import "./Router.css";
 import { Route, Routes } from "react-router-dom";
 import MainTeamJoinNo from "./components/MainTeamJoinNo";
 import MainTeamJoin from "./components/MainTeam";
-import Alarm from "./components/AlarmTeam";
-import Letter from "./components/LetterTeam";
+import MainTeamOwner from "./components/MainTeamOwner";
 import SidebarImg from "./svg/add.svg";
-import { useRef, useReducer, createContext, useCallback, useMemo } from "react";
+import {
+  useRef,
+  useReducer,
+  createContext,
+  useCallback,
+  useMemo,
+  useState,
+} from "react";
 
 const mocDateSide = [
   {
@@ -19,42 +25,137 @@ function reducer(state, action) {
   switch (action.type) {
     case "CREATE":
       return [action.data, ...state];
+    case "Del":
+      return state.filter((item) => item.id != action.targetId);
+    case "Find":
+      return state.filter((item) => item.id === action.targetId);
     default:
       return state;
   }
 }
 
 export const SidebarContext = createContext();
+export const UseStateContext = createContext();
+export const DiscordContext = createContext();
 
 export const AppRouter = () => {
   const [todos, dispatch] = useReducer(reducer, mocDateSide);
-  const idRef = useRef(1);
+  const idRefEven = useRef(2);
+  const idRefOdd = useRef(1);
 
-  const onCreateSidevar = useCallback((content) => {
+  const onCreateone = useCallback((content) => {
+    const newId = idRefEven.current;
+    idRefEven.current += 2;
     dispatch({
       type: "CREATE",
       data: {
-        id: idRef.current++,
+        id: newId,
         content: content,
         isPlus: false,
       },
     });
   }, []);
 
+  const onCreatedouble = useCallback((content) => {
+    const newId = idRefOdd.current;
+    idRefOdd.current += 2;
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: newId,
+        content: content,
+        isPlus: false,
+      },
+    });
+  }, []);
+
+  const onDel = useCallback((targetId) => {
+    dispatch({
+      type: "Del",
+      targetId: targetId,
+    });
+  }, []);
+
+  const onFind = useCallback((targetId) => {
+    dispatch({
+      type: "Find",
+      targetId: targetId,
+    });
+  }, []);
+
   const memoizedDispatch = useMemo(() => {
     return {
-      onCreateSidevar,
+      onCreateone,
+      onCreatedouble,
+      onDel,
+      onFind,
     };
   }, []);
 
+  const [setting, setSetting] = useState(false);
+  const [isAlarm, setIsAlarm] = useState(false);
+  const [isLetter, setIsLetter] = useState(false);
+  const [isFeedback, setIsFeedback] = useState(false);
+  const [isKeyword, setIsKeyword] = useState(false);
+  const [discord, setDiscord] = useState(false);
+  const [isMike, setIsMike] = useState(false);
+  const [isCamera, setIsCamera] = useState(false);
+  const [FullScreen, setFullScreen] = useState(false);
+  const [meetingEnd, setMeetingEnd] = useState(false);
+  const [isListening, setIsListening] = useState(false);
+  const [transcript, setTranscript] = useState("");
+  const videoRef = useRef(null);
+  const [stream, setStream] = useState(null);
+
   return (
-    <SidebarContext.Provider value={{ todos, dispatch: memoizedDispatch }}>
-      <Routes>
-        <Route path="/" element={<MainTeamJoinNo />} />
-        <Route path="/TeamJoin" element={<MainTeamJoin />} />
-        <Route path="/Alarm" element={<Alarm />} />
-        <Route path="/Letter" element={<Letter />} />
-      </Routes>
+    <SidebarContext.Provider
+      value={{
+        todos,
+        dispatch: memoizedDispatch,
+      }}
+    >
+      <UseStateContext.Provider
+        value={{
+          setting,
+          setSetting,
+          isAlarm,
+          setIsAlarm,
+          isLetter,
+          setIsLetter,
+          isFeedback,
+          setIsFeedback,
+          isKeyword,
+          setIsKeyword,
+          FullScreen,
+          setFullScreen,
+          discord,
+          setDiscord,
+          isMike,
+          setIsMike,
+          isCamera,
+          setIsCamera,
+          meetingEnd,
+          setMeetingEnd,
+        }}
+      >
+        <DiscordContext.Provider
+          value={{
+            isListening,
+            setIsListening,
+            transcript,
+            setTranscript,
+            videoRef,
+            stream,
+            setStream,
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<MainTeamJoinNo />} />
+            <Route path="/TeamJoin" element={<MainTeamJoin />} />
+            <Route path="/TeamOwner" element={<MainTeamOwner />} />
+          </Routes>
+        </DiscordContext.Provider>
+      </UseStateContext.Provider>
     </SidebarContext.Provider>
   );
 };
