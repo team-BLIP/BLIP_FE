dabd7a4d3960d5b4bb2f422fd7cc8a66af2e8bbd
tccmw <jungmwoo6248@gmail.com>
2025-03-18 19:01:36 +0900

    CORS 설정

--- blip-project/vite.config.js
+++ blip-project/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/external_api.js": {
+        target: "https://meet.jit.si",
+        changeOrigin: true,
+        rewrite: (path) =>
+          path.replace(/^\/external_api.js/, "/external_api.js"),
+      },
+    },
+  },
+});
+
