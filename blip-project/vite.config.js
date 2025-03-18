da67da83c1cfd44146c01a0f18aede17c514efec
seeum0507 <kwonstella0507@gmail.com>
2025-03-18 15:20:21 +0900

    세팅

--- blip-project/vite.config.js
+++ blip-project/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 8080,
+    proxy: {
+      "/users": {
+        target: "http://192.168.1.25:8080",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/users/, ""),
+      },
+    },
+  },
+});
