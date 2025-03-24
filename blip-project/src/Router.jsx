b3a29ecad720af84a631c2566467b4fd95590dbb
seeum0507 <kwonstella0507@gmail.com>
2025-03-24 22:53:45 +0900

    Route 경로추가

--- blip-project/src/Router.jsx
+++ blip-project/src/Router.jsx
@@ -1,13 +1,13 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Login from "./pages/Login/login";
 import Signup from "./pages/Signup/signUp";
 import SignupSuccess from "./pages/Signup/signupSuccess";
-import SignupVoice from "./pages/Signup/signupVoice";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/users/signup" element={<Signup />} />
         <Route path="/success" element={<SignupSuccess />} />
