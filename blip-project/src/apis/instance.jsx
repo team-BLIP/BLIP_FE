1445f98d7b848ec1504b26667a36a8582e8748ce
seeum0507 <kwonstella0507@gmail.com>
2025-03-28 16:13:35 +0900

    주석 IP 삭제

--- blip-project/src/apis/instance.jsx
+++ blip-project/src/apis/instance.jsx
@@ -1,13 +1,5 @@
 import axios from "axios";
 
-/* export const instance = axios.create({
-  baseURL: "http://192.168.1.25:8080", 
-  headers: {
-    "User-Agent": "Mozilla/5.0",
-    "Content-Type": "application/json",
-  },
-}); */
-
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_USER_BASE_URL,
   headers: {
