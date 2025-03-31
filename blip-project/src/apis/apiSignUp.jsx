075941eb97f51d0e1f191af2e50cb8f390406085
seeum0507 <kwonstella0507@gmail.com>
2025-03-31 19:32:27 +0900

    회원가입 연동

--- blip-project/src/apis/apiSignUp.jsx
+++ blip-project/src/apis/apiSignUp.jsx
@@ -2,6 +2,11 @@ import { instance } from "./instance";
 
 const apiSignUp = async (userData) => {
   try {
+    const data = {
+      id: inputs.id,
+      email: inputs.email,
+      password: inputs.password,
+    };
     const response = await instance.post("/users/signup", userData, {
       headers: {
         "Content-Type": "application/json",
@@ -13,21 +18,5 @@ const apiSignUp = async (userData) => {
     throw error.response?.data || error;
   }
 };
-
-/*const apiSignUp = async (userData) => {
-  try {
-    const response = await axios.post("/signup", userData, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("accessToken")}`, 
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error("회원가입 요청 실패:", error);
-    throw error;
-  }
-};
-*/
 
 export default apiSignUp;
--- blip-project/src/apis/instance.jsx
+++ blip-project/src/apis/instance.jsx
@@ -2,7 +2,4 @@ import axios from "axios";
 
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_USER_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
