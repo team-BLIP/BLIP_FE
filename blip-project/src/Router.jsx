c179387d24b8ff777b3cc15f0f9bde0f9cc8f2e0
seeum0507 <kwonstella0507@gmail.com>
2025-03-31 19:32:34 +0900

    router 경로

--- blip-project/src/Router.jsx
+++ blip-project/src/Router.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import Login from "./pages/Login/login";
-import Signup from "./pages/Signup/signUp";
+import Signup from "./pages/Signup/signup";
 import SignupSuccess from "./pages/Signup/signupSuccess";
 
 export const AppRouter = () => {
@@ -8,7 +8,7 @@ export const AppRouter = () => {
     <>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/users/login" element={<Login />} />
         <Route path="/users/signup" element={<Signup />} />
         <Route path="/success" element={<SignupSuccess />} />
       </Routes>
