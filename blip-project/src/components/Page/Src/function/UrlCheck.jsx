15da3bdef5acf97761a6cf699d952a10839d5dc9
정명우 <163850138+tccmw@users.noreply.github.com>
2025-04-06 22:39:37 +0900

    refactor: UrlCheck 함수 수정
    UrlCheck 함수의 로직을 변경하여 입력값 검증 방식을
    단순화하고, 외부 상태 관리 로직을 함수 내부에서
    처리하지 않도록 수정함
    
    - 이전에는 URL 객체 생성 시도를 통해 유효성을
      검사했으나, 이제는 입력값의 존재 여부만
      확인하도록 변경
    - setIsInput, setIsValidURL 상태 관리 함수를
      파라미터로 받아 사용하도록 변경하여 컴포넌트의
      재사용성 향상

--- blip-project/src/components/Page/Src/function/UrlCheck.jsx
+++ blip-project/src/components/Page/Src/function/UrlCheck.jsx
@@ -1,12 +1,9 @@
-const UrlCheck = (e) => {
-  const value = e.target.value;
-  setIsInput(value);
-  try {
-    new URL(value); 
-    setIsValidURL(true); 
-  } catch (error) {
-    setIsValidURL(false);
-  }
-};
+const UrlCheck = (setIsInput, setIsValidURL) => {
+  return (e) => {
+    const value = e.target.value;
+    setIsInput(value);
 
+    setIsValidURL(!!value.trim());
+  };
+};
 export default UrlCheck;
