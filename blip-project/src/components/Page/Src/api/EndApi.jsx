73c5d4801633c2116c3f69b4000f46d0fd480441
정명우 <163850138+tccmw@users.noreply.github.com>
2025-04-07 09:36:45 +0900

    feat: 회의 종료 API 구현
    회의 종료 API를 추가하여 팀 회의 종료 기능을 구현
    
    - `EndApi.jsx` 파일 생성 및 API 로직 구현
    - `TeamId`를 사용하여 회의 종료 요청 전송
    - 회의 종료 성공 시 응답 데이터 반환
    - 회의 종료 실패 시 에러 메시지 출력 및 alert 발생

---
+++ blip-project/src/components/Page/Src/api/EndApi.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const EndApi = async (TeamId) => {
+  const apiUrl = import.meta.env.VITE_API_URL_URL_MEETINGS_END;
+  const url = `${apiUrl}/data`;
+  const accessToken = "토큰 값";
+
+  const data = {
+    meeting_id: TeamId,
+    // leader_id: "Long",
+  };
+
+  try {
+    const response = await axios.post(url, data, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    console.log("회의 종료", response.data);
+    return response.data;
+  } catch (error) {
+    console.log("회의가 아직 안끝났습니다.", error);
+    alert("팀 회의가 아직 안끝났습니다. 다시 회의에 집중하세요!");
+  }
+};
+
+export default EndApi;
