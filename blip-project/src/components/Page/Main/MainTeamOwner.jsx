2a7a0e1e86c33a4ba27547fe6e696e15fc392f12
정명우 <163850138+tccmw@users.noreply.github.com>
2025-04-09 15:21:54 +0900

    refactor: MainTeam 컴포넌트 디버깅 코드 추가
    디버깅을 위해 MainTeam, MainTeamJoinNo, MainTeamOwner 컴포넌트에
    discord 상태 출력 코드를 추가
    
    - useContext를 사용하여 Router에서 discord 상태 값을 가져옴
    - console.log를 사용하여 컴포넌트에서 discord 상태 출력
    - 디버깅 시 discord 상태 확인 용이

--- blip-project/src/components/Page/Main/MainTeamOwner.jsx
+++ blip-project/src/components/Page/Main/MainTeamOwner.jsx
@@ -4,8 +4,14 @@ import HeaderTeam from "../Src/page/HeaderTeam";
 import SidebarTeam from "../Src/sidebarTeam";
 import StartTeam from "./StartTeam";
 import MeetingTeam from "../Src/MeetingTeam";
+import { useContext } from "react";
+import { UseStateContext } from "../../../Router"
 
 const MainTeamOwner = () => {
+  const { discord } = useContext(UseStateContext);
+
+  // 디버깅을 위한
+  console.log("MTeamJoinNo - discord 상태:", discord);
   return (
     <>
       <HeaderTeam />
