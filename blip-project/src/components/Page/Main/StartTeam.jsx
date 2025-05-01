7f86055c1520ba82b3a4a125e9d08b414a7eaf94
정명우 <163850138+tccmw@users.noreply.github.com>
2025-05-01 23:49:21 +0900

    fix: 불필요한 콘솔 로그 제거
    StartTeam 컴포넌트에서 사용하지 않는 콘솔 로그를 제거함
    - 디버깅 용도로 추가되었던 콘솔 로그를 삭제하여 불필요한 출력을 방지
    - 코드 가독성 향상

--- blip-project/src/components/Page/Main/StartTeam.jsx
+++ blip-project/src/components/Page/Main/StartTeam.jsx
@@ -13,10 +13,6 @@ import MainJoin from "./MainTeam";
 const StartTeam = () => {
   const { setting, isAlarm, isLetter, discord, basic, join } =
     useContext(UseStateContext);
-
-  console.log("dsfghasd", discord);
-
-  console.log("현재 값", setting, isAlarm, isLetter, discord, basic, join);
   return (
     <div className="start-main">
       <Member />
