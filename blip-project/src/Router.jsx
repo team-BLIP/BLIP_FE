1ee6ab520572d9c0e92faf4ad8f654bd9eb58ffb
seeum0507 <kwonstella0507@gmail.com>
2025-05-14 19:49:50 +0900

    router 적용

--- blip-project/src/Router.jsx
+++ blip-project/src/Router.jsx
@@ -1,7 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+import Login from "./components/Page/Login/login";
+import Signup from "./components/Page/Signup/signup";
+import SignupSuccess from "./components/Page/Signup/signupSuccess";
 
 export const AppRouter = () => {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/users/signup" element={<Signup />} />
+      <Route path="/success" element={<SignupSuccess />} />
+    </Routes>
+  );
 };
 
 export default AppRouter;
